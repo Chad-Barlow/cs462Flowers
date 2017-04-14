75a95b6750d6377e881240854cff6e1eb880c2d0
EC2 Default User <ec2-user@ip-172-31-5-217.us-west-2.compute.internal>
2017-04-14 04:21:39 +0000

    Added basic store page and foursquare auth

--- index.js
+++ index.js
@@ -1,10 +1,84 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var path = require('path');
+var connect = require('connect');
+var http = require('http');
+//var tools = require('./tools.js');
+var cookieParser = require('cookie-parser');
+var cookieSession = require('cookie-session');
+var session = require('client-sessions');
+var URI = require('urijs');
+var fs = require('fs');
+var bodyParser = require('body-parser');
+//var tools = require('./tools.js');
+var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
+app.use(express.static(__dirname + '/'));
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+
+app.use(session({
+  cookieName: 'session',
+  secret: "superduppersecretuuid20sdfsfd",  //tools.generateUUID(),
+  duration: 30 * 60 * 1000,
+  activeDuration: 5 * 60 * 1000,
+  ephemeral: true
+}));
+
+app.use(function(req, res, next) { 
+  //If we want to add sessions this is where it would go. 
+  /*if (req.session && req.session.user) {
+	if(user.userId === req.session.user.userId){
+               req.user = user;
+               req.session.user = user;
+               res.locals.user = user;
+        }
+    } else {
+      next();
+    }*/
+    console.log("Fake session created!");
+    next();
+});
+
+app.get('/', (req,res)=>{
+    res.redirect('login');
+});
+
+app.listen(3005, function () {
+    console.log('Example app listening on port 3005!');
 })
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
-})
+
+var config = {
+  'secrets' : {
+    'clientId' : 'USBHU4SJMV3UWRSGUHN1DNQ5234FRX2NTUEDT100GM4RETGO',
+    'clientSecret' : 'GOCNUL4BVWQZ0LMVXVX4RM4FVOY2YH4VP3MHC5TCUKZJHYXE',
+    'redirectUrl' : 'http://ec2-54-202-97-114.us-west-2.compute.amazonaws.com:3005/redirect'
+  }
+};
+
+var foursquare = require('node-foursquare')(config);
+
+//Use this one as the exchange thingy better than mine
+app.get('/login', function(req, res) {
+    res.writeHead(303, { 'location': foursquare.getAuthClientRedirectUrl() });
+    res.end();
+});
+
+//Finish foursquare login procedure
+app.get('/redirect', function (req, res) {
+  foursquare.getAccessToken({
+    code: req.query.code
+  }, function (error, accessToken) {
+    if(error) {
+      res.send('An error was thrown: ' + error.message);
+    }
+    else {
+      req.session.user = "store";
+      res.redirect('store');
+    }
+   });
+});
+
+app.get('/store',function (req,res){
+	res.sendFile(path.join(__dirname + '/store.html'));
+});
